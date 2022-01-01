2b1f949ef32ac3344e82d2c2d7359689327d562a
Dark Selur E.H.T <68709896+Kuronekonova@users.noreply.github.com>
2022-01-01 19:01:06 +0300

    More accurate conversions
    - Used OOP
    - More accurate values.

--- _tuc.py
+++ _tuc.py
@@ -1,7 +1,9 @@
 from tkinter import *
 from tkinter import ttk, messagebox
 import os
-timeUnits, tus_seconds, spaceStrings = ["Second(s)", "Minute(s)", "Hour(s)", "Day(s)", "Week(s)", "Month(s)", "Year(s)", "Decade(s)", "Century(s)"], [1, 60, 3600, 86400, 604800, 2419200, 29030400, 290304000, 2903040000], ["                                       ", "                                                                 "]
+timeUnits = ["Second(s)", "Minute(s)", "Hour(s)", "Day(s)", "Week(s)", "Month(s)", "Year(s)", "Decade(s)", "Century(s)"]
+tus_seconds = [1, 60, 3600, 86400, 604800, 2.628e+6, 3.154e+7, 3.154e+8, 3.154e+9]
+spaceStrings = ["                                       ", "                                                                 "]
 def findElement_index(array, num):
     if(num not in array):
         elementIndex = None
@@ -13,49 +15,85 @@ def findElement_index(array, num):
                 return elementIndex
             else:
                 continue
-def leftside_conversion(event): # Put the converted amount into the right side.
-    inputedAmount = leftSide_Entry.get()
+def leftside_conversion(event): 
+    inputedAmount = leftSide_Entry.entryWidget.get()
     inputedAmount = inputedAmount.replace(",", ".")
     try:
         inputedAmount = float(inputedAmount)
     except:
-        rightSide_Entry.delete(0, END)
+        rightSide_Entry.entryWidget.delete(0, END)
     conversionResult = (inputedAmount * tus_seconds[findElement_index(timeUnits, str(leftSide_option.get()))]) / tus_seconds[findElement_index(timeUnits, str(rightSide_option.get()))]
-    rightSide_Entry.delete(0, END)
-    rightSide_Entry.insert(0, str(conversionResult))
-def rightside_conversion(event): # Put the converted amount into the left side.
-    inputedAmount = rightSide_Entry.get()
+    rightSide_Entry.entryWidget.delete(0, END)
+    rightSide_Entry.entryWidget.insert(0, str(conversionResult))
+def rightside_conversion(event):
+    inputedAmount = rightSide_Entry.entryWidget.get()
     inputedAmount = inputedAmount.replace(",", ".")
     try:
         inputedAmount = float(inputedAmount)
     except:
-        leftSide_Entry.delete(0, END)
+        leftSide_Entry.entryWidget.delete(0, END)
     conversionResult = (inputedAmount * tus_seconds[findElement_index(timeUnits, str(rightSide_option.get()))]) / tus_seconds[findElement_index(timeUnits, str(leftSide_option.get()))]
-    leftSide_Entry.delete(0, END)
-    leftSide_Entry.insert(0, str(conversionResult))
-root = Tk()
-root.geometry("700x110")
-root.resizable(False, False)
-root.title("Time Units Convertor")
-equal_sign = ttk.Label(root, text="=")
-equal_sign.configure(font=("Times New Roman", 45))
-equal_sign.place(x=330, y=12)
-ttk.Label(root, text=spaceStrings[0] + "Select any unit you want, and input how much of that unit you want to convert on any side." + spaceStrings[1], background="gray").place(x=0)
-leftSide_Entry = ttk.Entry(root, width=45)
-leftSide_Entry.focus_force()
-leftSide_Entry.bind("<KeyRelease>", lambda event: leftside_conversion(leftSide_Entry))
-leftSide_Entry.place(x=5, y=30, height=30)
-rightSide_Entry = ttk.Entry(root, width=45)
-rightSide_Entry.bind("<KeyRelease>", lambda event: rightside_conversion(rightSide_Entry))
-rightSide_Entry.place(x=420, y=30, height=30)
+    leftSide_Entry.entryWidget.delete(0, END)
+    leftSide_Entry.entryWidget.insert(0, str(conversionResult))
+class windowWidget():
+    def __init__(self, mainWindow, windowTitle, windowHeight, windowWidth, trueState, falseState):
+        self.mainWindow = mainWindow
+        self.windowTitle = windowTitle
+        self.windowWidth = windowWidth
+        self.windowHeight = windowHeight
+        self.trueState = trueState
+        self.falseState = falseState
+        self.mainWindow.title(windowTitle)
+        self.mainWindow.geometry(str(windowWidth) + "x" + str(windowHeight))
+        self.mainWindow.resizable(trueState, falseState)
+        self.mainWindow.focus_force() 
+        self.mainWindow.mainloop = mainloop
+class entryWidget():
+    def __init__(self, entryWidget, entryY_coord, entryX_coord, entry_Width, entry_Height, entry_Command, allow_Focus, bindType):
+        self.entryWidget = entryWidget
+        self.entryY_coord = entryY_coord
+        self.entryX_coord = entryX_coord
+        self.entry_Width = entry_Width
+        self.entry_Height = entry_Height
+        self.entry_Command = entry_Command
+        self.allow_Focus = allow_Focus
+        self.bindType = bindType
+        self.entryWidget.place(x=entryX_coord, y=entryY_coord, height=entry_Height)
+        self.entryWidget.configure(width=entry_Width)
+        if(allow_Focus == True):
+            self.entryWidget.focus_force()
+        else:
+            pass
+        if(self.bindType == "<Return>"):
+            self.entryWidget.bind("<Return>", lambda event: self.entry_Command(self.entryWidget))
+        elif(self.bindType == "<KeyRelease>"):
+            self.entryWidget.bind("<KeyRelease>", lambda event: self.entry_Command(self.entryWidget))
+        else:
+            pass
+class labelWidget(): 
+    def __init__(self, labelWidget, labelX_coord, labelY_coord, label_text, label_font, label_bg, label_fg):
+        self.labelWidget = labelWidget
+        self.labelX_coord = labelX_coord
+        self.labelY_coord = labelY_coord
+        self.label_text = label_text
+        self.label_font = label_font
+        self.label_bg = label_bg
+        self.label_fg = label_fg
+        self.labelWidget.place(x=labelX_coord, y=labelY_coord)
+        self.labelWidget.configure(text=self.label_text, font=self.label_font, background=self.label_bg, foreground=self.label_fg)
+root = windowWidget(mainWindow=Tk(), windowTitle="Time Units Convertor", windowWidth=700, windowHeight=110, trueState=False, falseState=False)
+equal_sign = labelWidget(labelWidget = ttk.Label(root.mainWindow), labelX_coord=330, labelY_coord=12, label_text="=", label_font=("Times New Roman", 45), label_bg=None, label_fg=None)
+leftSide_Entry = entryWidget(entryWidget=ttk.Entry(root.mainWindow), entryY_coord=30, entryX_coord=5, entry_Width=45, entry_Height=30, entry_Command=lambda event: leftside_conversion(leftSide_Entry), allow_Focus=True, bindType="<KeyRelease>")
+rightSide_Entry = entryWidget(entryWidget=ttk.Entry(root.mainWindow), entryY_coord=30, entryX_coord=420, entry_Width=45, entry_Height=30, entry_Command=lambda event: rightside_conversion(rightSide_Entry), allow_Focus=False, bindType="<KeyRelease>")
+labelWidget(labelWidget = ttk.Label(root.mainWindow), labelX_coord=0, labelY_coord=0, label_text=spaceStrings[0] + "Select any unit you want, and input how much of that unit you want to convert on any side." + spaceStrings[1], label_font=None, label_bg="gray", label_fg=None)
 leftSide_option = StringVar()
 leftSide_option.set("Second(s)")
-leftSide_Menu = OptionMenu(root, leftSide_option, *timeUnits, command=lambda event: leftside_conversion(leftSide_Entry))
+leftSide_Menu = OptionMenu(root.mainWindow, leftSide_option, *timeUnits, command=lambda event: leftside_conversion(leftSide_Entry))
 leftSide_Menu.configure(width=39)
-leftSide_Menu.place(x=4, y=65)
 rightSide_option = StringVar()
 rightSide_option.set("Second(s)")
-rightSide_Menu = OptionMenu(root, rightSide_option, *timeUnits, command=lambda event: rightside_conversion(rightSide_Entry))
+rightSide_Menu = OptionMenu(root.mainWindow, rightSide_option, *timeUnits, command=lambda event: rightside_conversion(rightSide_Entry))
 rightSide_Menu.configure(width=39)
+leftSide_Menu.place(x=4, y=65)
 rightSide_Menu.place(x=420, y=65)
-root.mainloop()
+root.mainWindow.mainloop()
